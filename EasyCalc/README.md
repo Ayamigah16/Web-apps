d4d8d8bcb86b95dccc568830a643b902cfabdc61
Abraham Ayamigah <122880578+Ayamigah16@users.noreply.github.com>
2023-06-12 03:24:22 +0000

    EasyCalc updated

--- EasyCalc/README.md
+++ EasyCalc/README.md
@@ -9,16 +9,16 @@ This is a calculator project that allows users to perform basic arithmetic calcu
 * Clear: Clear the calculator's input field.
 
 ## Installation
-Clone the repository:  **https://github.com/Ayamigah16/Web-apps.git**
-Navigate to the project directory: cd **EasyCalc**
-Install dependencies: [insert instructions for installing dependencies here]
-Run the project: [insert instructions for running the project here]
+- Clone the repository:  **https://github.com/Ayamigah16/Web-apps.git**
+- Navigate to the project directory: cd **EasyCalc**
+- Run the project: [insert instructions for running the project here]
+
 
 ## Usage
 1. Enter the first number in the input field.
 2. Select the desired arithmetic operation (+, -, *, /).
 3. Enter the second number in the input field.
-4. Click the "Calculate" button to perform the calculation.
+4. Click the **"="** button to perform the calculation.
 5. The result will be displayed on the screen.
 6. To perform another calculation, click the "Clear" button to reset the input field.
 
--- EasyCalc/icalc.js
+++ EasyCalc/icalc.js
@@ -1,5 +1,5 @@
-class Calculator{
-    constructor( previousOperandTextElement, currentOperandTextElement){
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
         this.previousOperandTextElement = previousOperandTextElement
         this.currentOperandTextElement = currentOperandTextElement
         this.clear()
@@ -7,19 +7,19 @@ class Calculator{
 
     // DEFINING FUNCTIONS 
     // a fuction to the clear the screen
-    clear(){
+    clear() {
         this.previousOperand = ""
         this.currentOperand = ""
         this.operation = undefined
     }
 
     // a function to delete inputs
-    delete(){
+    delete() {
 
     }
 
     // a function to input values
-    appendNumber(number){
+    appendNumber(number) {
         // adding only one single period
         if (number == '.' && this.currentOperand.includes('.')) return
 
@@ -31,9 +31,9 @@ class Calculator{
     chooseOperation(operation) {
         if (this.currentOperand === '') return
 
-        if (this.previousOperand !== ''){
+        if (this.previousOperand !== '') {
             this.compute()
-        } 
+        }
 
         this.operation = this.operation
         this.previousOperand = this.currentOperand
@@ -41,13 +41,13 @@ class Calculator{
     }
 
     // a function to compute values
-    compute(){
+    compute() {
         let computation
         const prev = parseFloat(this.previousOperand)
         const current = parseFloat(this.currentOperand)
-        if(isNaN(prev) || isNaN(current)) return
+        if (isNaN(prev) || isNaN(current)) return
 
-        switch(this.operation){
+        switch (this.operation) {
             case '+':
                 computation = prev + current
                 break
@@ -59,9 +59,9 @@ class Calculator{
                 break
             case '*':
                 computation = prev * current
-                break    
+                break
             default:
-                break     
+                break
         }
 
         this.currentOperand = computation
@@ -70,7 +70,7 @@ class Calculator{
     }
 
     // a function to update the screen
-    updateDisplay(){
+    updateDisplay() {
         this.currentOperandTextElement.innerText = this.currentOperand
         this.previousOperandTextElement.innerText = this.previousOperand
     }
@@ -79,13 +79,13 @@ class Calculator{
 
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButtons = document.querySelector('[data-equals]')
+const equalsButton = document.querySelector('[data-equals]')
 const deleteButtons = document.querySelector('[data-delete]')
 const allclearButtons = document.querySelector('[data-allclear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
-const calculator = new Calculator(previousOperandTextElement,currentOperandTextElement)
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
@@ -101,4 +101,9 @@ operationButtons.forEach(button => {
     })
 })
 
-
+equalsButton.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.compute()
+        calculator.updateDisplay()
+    })
+})
